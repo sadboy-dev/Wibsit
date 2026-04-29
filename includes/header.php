408c850406d6b13f59b3d7794e47c57b92ba3ece
Jriik89 <110338305+sadboy-dev@users.noreply.github.com>
2026-04-29 13:43:06 +0800

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,7 @@
+<div style="background:black;padding:10px;display:flex;justify-content:space-between;color:gold;">
+  <div>XD4D</div>
+  <div>
+    👤 <?php echo $_SESSION['user']; ?> |
+    <a href="logout" style="color:red;">Logout</a>
+  </div>
+</div>
